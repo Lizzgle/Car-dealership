7250dc9c958d2112c4650072dacd9d1fdcfb4c09
lizzgle <lizaglebtsova@gmail.com>
2023-11-05 17:33:08 +0300

    add admin and user role

---
+++ CarShop.IdentifyServer/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarShop.IdentifyServer.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CarShop.IdentifyServer/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,9 @@
+
+namespace CarShop.IdentifyServer.Pages.Logout
+{
+    public class LogoutOptions
+    {
+        public static bool ShowLogoutPrompt = true;
+        public static bool AutomaticRedirectAfterSignOut = false;
+    }
+}
